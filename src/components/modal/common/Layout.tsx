3f163b1136188761d77a162925c304257fbd2602
sweeeeetpotato <sweet_potato13@naver.com>
2023-07-24 16:34:04 +0900

    Update: 모달창 레이아웃에 모달 제목 포함

--- src/components/modal/common/Layout.tsx
+++ src/components/modal/common/Layout.tsx
@@ -3,10 +3,11 @@ import { ReactNode } from "react";
 
 interface ModalProps {
   handleBtn: () => void;
+  modalTitle: string[];
   children: ReactNode;
 }
 
-const Layout: React.FC<ModalProps> = ({ handleBtn, children }) => {
+const Layout: React.FC<ModalProps> = ({ handleBtn, modalTitle, children }) => {
   return (
     <div
       className="fixed top-0 left-0 w-screen h-screen bg-black/30 flex flex-col justify-center items-center"
@@ -16,6 +17,20 @@ const Layout: React.FC<ModalProps> = ({ handleBtn, children }) => {
         className="relative w-[770px] shadow-2xl bg-white pt-10 px-8 pb-9 flex flex-col gap-5 box-border rounded-[10px]"
         onClick={e => e.stopPropagation()}
       >
+        <p className="flex gap-[10px] text-xl font-bold text-grayscale-100">
+          {modalTitle.map((val, idx) =>
+            idx === 0 ? (
+              <span key={idx}>{val}</span>
+            ) : (
+              <span
+                key={idx}
+                className="relative pl-[17px] before:absolute before:top-[9px] before:left-0 before:w-[7px] before:h-[11px] before:bg-[url('/images/right-arrow.svg')] before:bg-no-repeat"
+              >
+                {val}
+              </span>
+            ),
+          )}
+        </p>
         {children}
         <button type="button">
           <Image
