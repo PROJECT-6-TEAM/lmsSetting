fd56a67a8e740938100dde0ab57457b506c7a9ea
leeyulgok <dbfrhr20@gmail.com>
2023-07-26 12:20:05 +0900

    Feat: Firebase 관련한 파일(백엔드 코드, 설정 파일 등)

---
+++ functions/src/index.ts
@@ -0,0 +1,5 @@
+import * as lectureApi from "./apis/lecture/lecture";
+import * as commentApi from "./apis/lecture/comments";
+
+export const getLecture = lectureApi.getLecture;
+export const getComments = commentApi.getComments;
