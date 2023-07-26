295eecd563a4650f3f295bd27bc8d20a5f34ddfa
jeongdopark <jeongdo.dev@gmail.com>
2023-07-26 16:58:06 +0900

    Chore: branch test

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,5 @@ export const fetchClient = async (endpoint: string, options?: RequestInit): Prom
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${endpoint}`, options);
   return response.json();
 };
+
+// branch test branch testbranch testbranch testbranch testbranch test
