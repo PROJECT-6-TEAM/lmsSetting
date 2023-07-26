505694aaf565ad1fd165e2fdbb235fd0f0f24d46
jeongdopark <jeongdo.dev@gmail.com>
2023-07-26 15:05:06 +0900

    Feat: custom fetch, react-query

---
+++ src/api/lecture.ts
@@ -0,0 +1,13 @@
+import { fetchClient } from "."
+
+export const getLectureList = async (lectureId:any) => {
+  try {
+    const response = await fetchClient(`/getLecture?lectureId=${lectureId}`, {
+      method: 'get',
+    });
+    return response
+  } catch(err){
+    console.log(err)
+  }
+}
+
